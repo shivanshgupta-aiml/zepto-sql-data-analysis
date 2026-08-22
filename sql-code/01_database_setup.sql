cdbe02b77a4aa90ae959fde3f59d58ac5c11c281
Shivansh Gupta <shivanshgupta2005.sg@gmail.com>
2026-08-22 10:39:09 +0530

    Update 01_database_setup.sql

--- sql-code/01_database_setup.sql
+++ sql-code/01_database_setup.sql
@@ -1,1 +1,41 @@
+drop table if exists zepto;
 
+create table zepto (
+sku_id SERIAL PRIMARY KEY,
+category VARCHAR(120),
+name VARCHAR(150) NOT NULL,
+mrp NUMERIC(8,2),
+discountPercent NUMERIC(5,2),
+availableQuantity INTEGER,
+discountedSellingPrice NUMERIC(8,2),
+weightInGms INTEGER,
+outOfStock BOOLEAN,	
+quantity INTEGER
+);
+
+
+-- In case you are facing 0 records imported error try this..
+
+  -- error handled
+TRUNCATE TABLE zepto;
+
+LOAD DATA LOCAL INFILE 'D:/Downloads/Cgrome downloads/zepto_v2.csv'
+INTO TABLE zepto
+CHARACTER SET latin1
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS
+(category,
+ name,
+ mrp,
+ discountPercent,
+ availableQuantity,
+ discountedSellingPrice,
+ weightInGms,
+ outOfStock,
+ quantity); 
+ 
+ ALTER TABLE zepto
+CONVERT TO CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
