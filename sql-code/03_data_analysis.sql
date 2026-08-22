f64b8815bd6092ce0c1093e8ea5876b145e1b94b
Shivansh Gupta <shivanshgupta2005.sg@gmail.com>
2026-08-22 10:49:51 +0530

    Add data exploration queries for zepto table
    Added SQL queries for data exploration, including row count, sample data, null value checks, distinct categories, stock status, and duplicate product names.

--- sql-code/03_data_analysis.sql
+++ sql-code/03_data_analysis.sql
@@ -1,1 +1,45 @@
+-- data exploration
 
+-- count of rows
+select count(*) from zepto;
+
+-- sample data
+SELECT * FROM zepto
+LIMIT 10;
+
+-- null values
+SELECT * FROM zepto
+WHERE name IS NULL
+OR
+category IS NULL
+OR
+mrp IS NULL
+OR
+discountPercent IS NULL
+OR
+discountedSellingPrice IS NULL
+OR
+weightInGms IS NULL
+OR
+availableQuantity IS NULL
+OR
+outOfStock IS NULL
+OR
+quantity IS NULL;
+
+-- different product categories
+SELECT DISTINCT category
+FROM zepto
+ORDER BY category;
+
+-- products in stock vs out of stock
+SELECT outOfStock, COUNT(sku_id)
+FROM zepto
+GROUP BY outOfStock;
+
+-- product names present multiple times
+SELECT name, COUNT(sku_id) AS "Number of SKUs"
+FROM zepto
+GROUP BY name
+HAVING count(sku_id) > 1
+ORDER BY count(sku_id) DESC;
