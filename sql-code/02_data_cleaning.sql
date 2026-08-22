6958e9769f4db58323f83b5f556647d91ff96167
Shivansh Gupta <shivanshgupta2005.sg@gmail.com>
2026-08-22 10:48:40 +0530

    Add data cleaning queries for product prices
    Added SQL queries for data cleaning, including deleting products with zero price and converting prices from paise to rupees.

--- sql-code/02_data_cleaning.sql
+++ sql-code/02_data_cleaning.sql
@@ -1,1 +1,17 @@
+-- data cleaning
 
+-- products with price = 0
+SELECT * FROM zepto
+WHERE mrp = 0 OR discountSellingPrice = 0;
+
+delete from zepto
+where mrp = 0;
+
+-- Convert paise to rupees 
+update zepto 
+set mrp = mrp/100.0,
+discountedSellingPrice = discountedSellingPrice/100.0
+where sku_id > 0;
+
+select mrp, discountedSellingPrice
+from zepto;
